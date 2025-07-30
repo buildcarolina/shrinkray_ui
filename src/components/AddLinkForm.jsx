059d4dc27bc8568c3928029d243a5fca609db577
Sean R Reid <sean@torchcodelab.com>
2025-07-30 11:24:37 -0400

    Added bearer token to Add Link request

--- src/components/AddLinkForm.jsx
+++ src/components/AddLinkForm.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { nanoid } from 'nanoid';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { nanoid } from "nanoid";
 
-import styles from './form.module.css';
+import styles from "./form.module.css";
 
 const AddLinkForm = () => {
-  const [linkName, setLinkName] = useState('');
-  const [linkUrl, setLinkUrl] = useState('');
+  const [linkName, setLinkName] = useState("");
+  const [linkUrl, setLinkUrl] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
@@ -14,6 +14,7 @@ const AddLinkForm = () => {
 
     const shortUrl = nanoid(6);
     const apiUrl = `${import.meta.env.VITE_API_URL}/urls/add`;
+    const access_token = localStorage.getItem("access_token");
 
     const body = {
       long_url: linkUrl,
@@ -24,28 +25,33 @@ const AddLinkForm = () => {
 
     try {
       const response = await fetch(apiUrl, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(body),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
         },
       });
       const data = await response.json();
-      console.log('DATA: ', data);
-      navigate('/links');
+      console.log("DATA: ", data);
+      navigate("/links");
     } catch (error) {
       console.error(error);
-      navigate('/404');
+      navigate("/404");
     }
   };
 
   return (
-		<form className={styles.form} method='POST' onSubmit={(e) => handleSubmit(e)}>
+    <form
+      className={styles.form}
+      method="POST"
+      onSubmit={(e) => handleSubmit(e)}
+    >
       <label>
         Name of Link
         <input
-          type='text'
-          name='linkName'
+          type="text"
+          name="linkName"
           value={linkName}
           onChange={(e) => setLinkName(e.target.value)}
         />
@@ -53,13 +59,13 @@ const AddLinkForm = () => {
       <label>
         Url of Link
         <input
-          type='text'
-          name='linkUrl'
+          type="text"
+          name="linkUrl"
           value={linkUrl}
           onChange={(e) => setLinkUrl(e.target.value)}
         />
       </label>
-      <button type='submit'>ZAP IT!</button>
+      <button type="submit">ZAP IT!</button>
     </form>
   );
 };
