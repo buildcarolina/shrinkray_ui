68268f278decd32ebdf4e5f0ececd78dc1b2b70f
Sean R Reid <sean@torchcodelab.com>
2025-07-30 08:58:41 -0400

    Added Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from "react-router";
 
-import { AuthProvider } from './AuthContext';
+import { AuthProvider } from "./AuthContext";
 
-import MainLayout from './layouts/MainLayout';
-import Login from './pages/Login';
-import Register from './pages/Register';
+//  Layouts
+import MainLayout from "./layouts/MainLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+
+// Forms
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Logout from "./pages/Logout";
-import Home from './pages/Home';
-import Links from './pages/Links';
-import AddLink from './pages/AddLink';
-import Broke from './pages/404';
+import AddLink from "./pages/AddLink";
+
+// Pages
+import Home from "./pages/Home";
+import Links from "./pages/Links";
+import Broke from "./pages/404";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/links' element={<Links />} />
-          <Route path='/addLink' element={<AddLink />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path="/logout" element={<Logout/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
-        <Route path='/404' element={<Broke />} />
+        <Route element={<ProtectedLayout />}>
+          <Route path="/links" element={<Links />} />
+          <Route path="/addLink" element={<AddLink />} />
+          <Route path="/logout" element={<Logout />} />
+        </Route>
+        <Route path="/404" element={<Broke />} />
       </Routes>
     </AuthProvider>
   );
